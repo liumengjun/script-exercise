daed5b840d4ed8911d913df3f12efc87df8f941b
Mengjun Liu <mengjun_liu@126.com>
2015-07-23 16:45:54 +0800

    云片网-发短信-测试脚本(shell)

---
+++ sh/test.yunpian.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+#author jacky
+#修改为您的apikey
+apikey="ha_ha_mine_bu_neng_xie_zai_zhe"
+echo $apikey
+#修改为您要发送的手机号
+mobile="18613888646"
+#设置您要发送的内容
+text="【云片网】您的验证码是1234"
+echo "get user info:"
+echo "http://yunpian.com/v1/user/get.json?apikey=$apikey"
+curl "http://yunpian.com/v1/user/get.json?apikey=$apikey"
+echo "\nsend sms:"
+curl --data "apikey=$apikey&mobile=$mobile&text=$text" "http://yunpian.com/v1/sms/send.json"
+
