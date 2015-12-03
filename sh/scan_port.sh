8bd9e2fae9bf5e18ba5838b0b7d0a5073c73f9d4
Mengjun Liu <mengjun_liu@126.com>
2015-12-03 11:40:00 +0800

    scan port shell

---
+++ sh/scan_port.sh
@@ -0,0 +1,11 @@
+if [ "$1" = "" ]; then echo "no host"; exit; fi
+if [ "$2" = "" ]; then echo "no start port"; exit; fi
+if [ "$3" = "" ]; then echo "no end port"; exit; fi
+host="$1"
+start="$2"
+end="$3"
+if [ $start -lt 1 ]; then start=1; fi
+if [ $end -lt 1 ]; then end=1; fi
+seq $start $end > /dev/null 2>&1
+if [ $? != 0 ]; then echo "params error"; exit; fi
+for port in `seq $start $end`; do nc -v -w 2 $host $port > /dev/null 2>&1; if [ $? = 0 ]; then echo $port; fi; done
