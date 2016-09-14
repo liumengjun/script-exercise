b290ebfd3dab365615cf66f507703d30b0e51407
Mengjun Liu <mengjun_liu@126.com>
2016-09-14 12:26:00 +0800

    change random_string scripts

--- py/random_string.py
+++ py/random_string.py
@@ -5,8 +5,15 @@ import random
 _chars = string.ascii_letters + string.digits
 
 def random_chars(num):
-    return ''.join(random.sample(_chars, num))
+    return ''.join(random.choice(_chars) for i in range(num))
 
 if __name__ == "__main__":
-    print(random_chars(32));
+    import sys
+    num = 32
+    if len(sys.argv) == 2:
+        num = int(sys.argv[1])
+        if num <1 or num > 4096:
+            print("%s is a wrong number in [1,4096]"%num)
+            quit()
+    print(random_chars(num));
 
--- sh/random_strings.sh
+++ sh/random_strings.sh
@@ -4,6 +4,15 @@ LC_ALL=C;
 # cat /dev/urandom | sed 's/[^a-zA-Z0-9]//g' | head
 #
 n=32
+if [ "$1" != "" ]; then
+  expr $1 "+" 10 &> /dev/null
+  if [ $? -eq 0 -a $1 -ge 1 -a $1 -le 256 ]; then
+    n=$1
+  else
+    echo "$1 is a wrong number in [1,256]"
+    exit -1
+  fi
+fi
 # rand_line=`cat /dev/urandom | sed 's/[^a-zA-Z0-9]//g' | strings -n $n | head -1`
 # echo ${rand_line:0:n}
 
