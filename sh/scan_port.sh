751fbe219fc6e7a1be9e47e85bff47ab27bcd1cd
Mengjun Liu <mengjun_liu@126.com>
2017-03-20 15:01:09 +0800

    update scan_post.sh script

--- sh/scan_port.sh
+++ sh/scan_port.sh
@@ -1,3 +1,5 @@
+# !/bin/sh
+# scan_port.sh: scan port, host:start~end
 if [ "$1" = "" ]; then echo "no host"; exit; fi
 if [ "$2" = "" ]; then echo "no start port"; exit; fi
 if [ "$3" = "" ]; then echo "no end port"; exit; fi
@@ -8,4 +10,10 @@ if [ $start -lt 1 ]; then start=1; fi
 if [ $end -lt 1 ]; then end=1; fi
 seq $start $end > /dev/null 2>&1
 if [ $? != 0 ]; then echo "params error"; exit; fi
-for port in `seq $start $end`; do nc -v -w 2 $host $port > /dev/null 2>&1; if [ $? = 0 ]; then echo $port; fi; done
+for port in `seq $start $end`; do
+    # echo $port;
+    nc -v -G 2 -w 2 $host $port > /dev/null 2>&1;
+    if [ $? = 0 ]; then
+        echo "$port is open";
+    fi;
+done
